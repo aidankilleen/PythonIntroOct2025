7be3bf387ec605dbe2a22a8c0431f1cc8d5dfc45
Aidan Killeen <aidan.killeen@gmail.com>
2025-10-30 12:29:11 +0000

    working on word_count.py

--- word_count.py
+++ word_count.py
@@ -1,30 +1,34 @@
 # word_count.py
-
 # count the number of words / lines / characters in a file
+
+# how do we implement 
+# options
+# -l - return the line count
+# -c - return the character count
+# -w - return the word count
 
 import sys
 
 print ("Word counter")
-print (sys.argv)
 try:
     file_name = sys.argv[1]
 
     with open(file_name, "r") as f:
-        lines = f.readlines()
-        characters = 0
+        line_count = f.readlines()
+        character_count = 0
         word_count = 0
 
-        for line in lines:
-            characters = characters + len(line)
-
+        for line in line_count:
+            character_count += len(line)
             words = line.split()
-            word_count = word_count + len(words)
+            word_count += len(words)
 
-        print (f"lines = {len(lines)}")
-        print (f"characters = {characters}")
+        print (f"lines = {len(line_count)}")
+        print (f"characters = {character_count}")
         print (f"words = {word_count}")
 
-
+except FileNotFoundError:
+    print (f"File {file_name} not found")
 except:
     print ("Usage:")
     print ("python word_count.py FILE")
